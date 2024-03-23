7cbe856db8943a5b270b4c3dbe7375462640368f
Rabzizz <amiral98.jeux@gmail.com>
2024-03-23 12:17:32 +0100

    add build function

--- Assets/Player/Builder/BuilderController.cs
+++ Assets/Player/Builder/BuilderController.cs
@@ -1,25 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BuilderController : MonoBehaviour
 {
+
+    public InputActionReference InputBuild;
     public LayerMask layerMasks;
     public float maxDistance;
+    public List<GameObject> items;
+    public int selectedItem = 0;
+
+    RaycastHit hit;
+
+    void Start()
+    {
+        InputBuild.action.performed += (_) => Build();
+    }
 
     private void FixedUpdate()
     {
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, layerMasks))
-        {
-            Debug.Log("Did Hit");
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
-            Debug.Log("Did not Hit");
-        }
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, layerMasks);
+    }
+
+    public void Build()
+    {
+        Instantiate(items[selectedItem], hit.point, Quaternion.Euler(-hit.normal));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hit.point, 0.1f);
     }
 }
