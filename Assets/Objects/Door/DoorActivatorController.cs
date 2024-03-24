5cd70dcf1a602d8acab384841185384461ff646f
Simon Meier <simon.meier@he-arc.ch>
2024-03-24 05:18:05 +0100

    Add minimap with walls and following entity position.

--- Assets/Objects/Door/DoorActivatorController.cs
+++ Assets/Objects/Door/DoorActivatorController.cs
@@ -5,6 +5,12 @@ public class DoorActivatorController : MonoBehaviour
     public DoorController door;
     public MinimapManager minimap;
 
+    private void Start()
+    {
+        if(transform.GetComponentInParent<MinimapManager>() != null)
+            minimap = transform.GetComponentInParent<MinimapManager>();
+    }
+
     private void OnMouseDown()
     {
         Debug.Log("Door activated");
